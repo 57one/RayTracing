b48b3967a6213587518b044c2a5947f4d267f08f
57one <784537055@qq.com>
2022-04-09 23:26:25 +0800

    add time

--- ray tracing in next week/ray.hpp
+++ ray tracing in next week/ray.hpp
@@ -4,22 +4,24 @@
 #include "vec.hpp"
 
 class ray {
-    public:
-        ray() {}
-        ray(const Point3d& origin, const Vec3d& direction)
-            : orig(origin), dir(direction)
-        {}
+   public:
+    ray() {}
+    ray(const Point3d &origin, const Vec3d &direction, double time = 0.f)
+        : orig(origin), dir(direction), tm(time) {
+    }
 
-        Point3d origin() const  { return orig; }
-        Vec3d direction() const { return dir; }
+    Point3d origin() const { return orig; }
+    Vec3d direction() const { return dir; }
+    double time() const { return tm; }
 
-        Point3d at(double t) const {
-            return orig + t*dir;
-        }
+    Point3d at(double t) const {
+        return orig + t * dir;
+    }
 
-    public:
-        Point3d orig;
-        Vec3d dir;
+   public:
+    Point3d orig;
+    Vec3d dir;
+    double tm;
 };
 
 #endif
