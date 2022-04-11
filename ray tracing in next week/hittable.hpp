12d257b00ee8755c0e57b8546bba27da9fd6072f
57one <784537055@qq.com>
2022-04-11 10:18:18 +0800

    add (u,v) coordinate to get texture

--- ray tracing in next week/hittable.hpp
+++ ray tracing in next week/hittable.hpp
@@ -12,6 +12,8 @@ struct hit_record {
     Vec3d normal;
     shared_ptr<material> mat_ptr;
     double t;
+    double u;
+    double v;
     bool front_face;
 
     inline void set_face_normal(const ray& r, const Vec3d& outward_normal) {
