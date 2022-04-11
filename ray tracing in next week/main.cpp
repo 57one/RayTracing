0f7413cddf505da8780497f0b9a7e16e69cf6602
57one <784537055@qq.com>
2022-04-11 10:47:35 +0800

    add checker_texture

--- ray tracing in next week/main.cpp
+++ ray tracing in next week/main.cpp
@@ -11,6 +11,9 @@
 hittable_list random_scene() {
     hittable_list world;
 
+    auto checker = make_shared<checker_texture>(Color3d(0.2, 0.3, 0.1), Color3d(0.9, 0.9, 0.9));
+    world.add(make_shared<sphere>(Point3d(0, -1000, 0), 1000, make_shared<lambertian>(checker)));
+
     auto ground_material = make_shared<lambertian>(Color3d(0.5, 0.5, 0.5));
     world.add(make_shared<sphere>(Point3d(0, -1000, 0), 1000, ground_material));
 
--- ray tracing in next week/texture.hpp
+++ ray tracing in next week/texture.hpp
@@ -4,24 +4,47 @@
 #include "rtweekend.hpp"
 
 class texture {
-    public:
-        virtual Color3d value(double u, double v, const Point3d& p) const = 0;
+   public:
+    virtual Color3d value(double u, double v, const Point3d& p) const = 0;
 };
 
 class solid_color : public texture {
-    public:
-        solid_color() {}
-        solid_color(Color3d c) : color_value(c) {}
+   public:
+    solid_color() {}
+    solid_color(Color3d c) : color_value(c) {}
 
-        solid_color(double red, double green, double blue)
-          : solid_color(Color3d(red,green,blue)) {}
+    solid_color(double red, double green, double blue)
+        : solid_color(Color3d(red, green, blue)) {}
 
-        virtual Color3d value(double u, double v, const Vec3d& p) const override {
-            return color_value;
-        }
+    virtual Color3d value(double u, double v, const Vec3d& p) const override {
+        return color_value;
+    }
 
-    private:
-        Color3d color_value;
+   private:
+    Color3d color_value;
+};
+
+class checker_texture : public texture {
+   public:
+    checker_texture() {}
+
+    checker_texture(shared_ptr<texture> _even, shared_ptr<texture> _odd)
+        : even(_even), odd(_odd) {}
+
+    checker_texture(Color3d c1, Color3d c2)
+        : even(make_shared<solid_color>(c1)), odd(make_shared<solid_color>(c2)) {}
+
+    virtual Color3d value(double u, double v, const Point3d& p) const override {
+        auto sines = sin(10 * p.x) * sin(10 * p.y) * sin(10 * p.z);
+        if (sines < 0)
+            return odd->value(u, v, p);
+        else
+            return even->value(u, v, p);
+    }
+
+   public:
+    shared_ptr<texture> odd;
+    shared_ptr<texture> even;
 };
 
 #endif
