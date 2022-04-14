ccb83775209599ac5d496964aa85946a12fcec33
57one <784537055@qq.com>
2022-04-14 22:23:30 +0800

    final scene

--- ray tracing in next week/main.cpp
+++ ray tracing in next week/main.cpp
@@ -162,6 +162,67 @@ hittable_list cornell_smoke() {
     return objects;
 }
 
+hittable_list final_scene() {
+    hittable_list boxes1;
+    auto ground = make_shared<lambertian>(Color3d(0.48, 0.83, 0.53));
+
+    const int boxes_per_side = 20;
+    for (int i = 0; i < boxes_per_side; i++) {
+        for (int j = 0; j < boxes_per_side; j++) {
+            auto w = 100.0;
+            auto x0 = -1000.0 + i * w;
+            auto z0 = -1000.0 + j * w;
+            auto y0 = 0.0;
+            auto x1 = x0 + w;
+            auto y1 = random_double(1, 101);
+            auto z1 = z0 + w;
+
+            boxes1.add(make_shared<box>(Point3d(x0, y0, z0), Point3d(x1, y1, z1), ground));
+        }
+    }
+
+    hittable_list objects;
+
+    objects.add(make_shared<bvh_node>(boxes1, 0, 1));
+
+    auto light = make_shared<diffuse_light>(Color3d(7, 7, 7));
+    objects.add(make_shared<xz_rect>(123, 423, 147, 412, 554, light));
+
+    auto center1 = Point3d(400, 400, 200);
+    auto center2 = center1 + Vec3d(30, 0, 0);
+    auto moving_sphere_material = make_shared<lambertian>(Color3d(0.7, 0.3, 0.1));
+    objects.add(make_shared<moving_sphere>(center1, center2, 0, 1, 50, moving_sphere_material));
+
+    objects.add(make_shared<sphere>(Point3d(260, 150, 45), 50, make_shared<dielectric>(1.5)));
+    objects.add(make_shared<sphere>(
+        Point3d(0, 150, 145), 50, make_shared<metal>(Color3d(0.8, 0.8, 0.9), 1.0)));
+
+    auto boundary = make_shared<sphere>(Point3d(360, 150, 145), 70, make_shared<dielectric>(1.5));
+    objects.add(boundary);
+    objects.add(make_shared<constant_medium>(boundary, 0.2, Color3d(0.2, 0.4, 0.9)));
+    boundary = make_shared<sphere>(Point3d(0, 0, 0), 5000, make_shared<dielectric>(1.5));
+    objects.add(make_shared<constant_medium>(boundary, .0001, Color3d(1, 1, 1)));
+
+    auto emat = make_shared<lambertian>(make_shared<image_texture>("earthmap.jpg"));
+    objects.add(make_shared<sphere>(Point3d(400, 200, 400), 100, emat));
+    auto pertext = make_shared<noise_texture>(0.1);
+    objects.add(make_shared<sphere>(Point3d(220, 280, 300), 80, make_shared<lambertian>(pertext)));
+
+    hittable_list boxes2;
+    auto white = make_shared<lambertian>(Color3d(.73, .73, .73));
+    int ns = 1000;
+    for (int j = 0; j < ns; j++) {
+        boxes2.add(make_shared<sphere>(Point3d::random(0, 165), 10, white));
+    }
+
+    objects.add(make_shared<translate>(
+        make_shared<rotate_y>(
+            make_shared<bvh_node>(boxes2, 0.0, 1.0), 15),
+        Vec3d(-100, 270, 395)));
+
+    return objects;
+}
+
 double hit_sphere(const Point3d& center, double radius, const ray& r) {
     Vec3d oc = r.origin() - center;
     auto a = r.direction().length_squared();
@@ -263,13 +324,23 @@ int main() {
             lookat = Point3d(278, 278, 0);
             vfov = 40.0;
             break;
-        default:
         case 7:
             world = cornell_smoke();
             aspect_ratio = 1.0;
             image_width = 600;
             samples_per_pixel = 200;
             lookfrom = Point3d(278, 278, -800);
+            lookat = Point3d(278, 278, 0);
+            vfov = 40.0;
+            break;
+        default:
+        case 8:
+            world = final_scene();
+            aspect_ratio = 1.0;
+            image_width = 800;
+            samples_per_pixel = 500;
+            background = Color3d(0, 0, 0);
+            lookfrom = Point3d(478, 278, -600);
             lookat = Point3d(278, 278, 0);
             vfov = 40.0;
             break;
